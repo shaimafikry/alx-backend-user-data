6bbae0c09096f0fc02cf03b3f5a03bb9e95f1bdd
shaimaa <shaimafikry@yahoo.com>
2024-09-08 01:55:48 +0300

    add2

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Module of auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ class of Auth"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ bool function"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user"""
+        return None
