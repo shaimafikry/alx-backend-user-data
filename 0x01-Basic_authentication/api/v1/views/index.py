87b041c9567ae63742a09bfb8273e83faa338d96
shaimaa <shaimafikry@yahoo.com>
2024-09-08 01:28:44 +0300

    add1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,6 +13,7 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
@@ -20,6 +21,7 @@ def unauthorized() -> str:
       - abort
     """
     abort(401)
+
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
