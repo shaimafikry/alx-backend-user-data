2e8c604dae41879d4dd2bd8bf8458879f66b19e9
shaimaa <shaimafikry@yahoo.com>
2024-09-08 05:18:13 +0300

    add2

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Module of auth class
+"""
+from api.v1.auth.auth import Auth
+from base64 import b64decode
+from models.user import User
+from typing import TypeVar
+
+
+class SessionAuth:
+    """ class auth"""
+    pass
