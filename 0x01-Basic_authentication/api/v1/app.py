341316eee197436fa12fa49d3007567ad86a243c
shaimaa <shaimafikry@yahoo.com>
2024-09-08 03:39:34 +0300

    add2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
+from api.v1.auth.auth_sec import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
---
+++ 0x01-Basic_authentication/api/v1/auth/auth_sec.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Module of auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ class of Auth"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ bool function"""
+        if not path:
+            return True
+        if not excluded_paths or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        if path not in excluded_paths:
+            return True
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ header"""
+        if request is None:
+            return None
+        head = request.headers.get('Authorization')
+        if not head:
+            return None
+        return head
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user"""
+        return None
